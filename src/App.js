49f4018e239adfb322bac114ba81f7fb2bb80099
akinyiliz <lizzieke84@gmail.com>
2023-01-04 13:24:17 +0300

    webpage structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import './App.css';
+import Navbar from './components/Navbar';
+import Shop from './pages/shop/Shop';
+import Cart from './pages/cart/Cart';
 
 function App() {
-	return <div>hello</div>;
+	return (
+		<div className="App">
+			<Router>
+				<Navbar />
+				<Routes>
+					<Route
+						path="/"
+						element={<Shop />}
+					/>
+					<Route
+						path="/cart"
+						element={<Cart />}
+					/>
+				</Routes>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
