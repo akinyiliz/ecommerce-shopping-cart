8c47f81af53f1fb2c6981298b626f732b67e983d
akinyiliz <lizzieke84@gmail.com>
2023-01-04 13:25:47 +0300

    array of products

---
+++ src/products.js
@@ -0,0 +1,59 @@
+import smartphone from './assets/smartphone.png';
+import laptop from './assets/laptop.png';
+import camera from './assets/camera.png';
+import dress from './assets/dress.png';
+import denimjacket from './assets/denim-jacket.png';
+import menshoes from './assets/men-shoes.png';
+import tv from './assets/tv.png';
+import heels from './assets/heels.png';
+
+export const PRODUCTS = [
+	{
+		id: 1,
+		productName: 'Smartphone',
+		price: 999.0,
+		productImage: smartphone,
+	},
+	{
+		id: 2,
+		productName: 'Laptop',
+		price: 1999.0,
+		productImage: laptop,
+	},
+	{
+		id: 3,
+		productName: 'Digital Camera',
+		price: 799.0,
+		productImage: camera,
+	},
+	{
+		id: 4,
+		productName: 'Dress',
+		price: 129.0,
+		productImage: dress,
+	},
+	{
+		id: 5,
+		productName: 'Denim Jacket',
+		price: 299.0,
+		productImage: denimjacket,
+	},
+	{
+		id: 6,
+		productName: 'Mens Shoes',
+		price: 399.0,
+		productImage: menshoes,
+	},
+	{
+		id: 7,
+		productName: 'Smart TV',
+		price: 2299.0,
+		productImage: tv,
+	},
+	{
+		id: 8,
+		productName: 'Red Heels',
+		price: 399.0,
+		productImage: heels,
+	},
+];
