79ee9b20eb2f59081be546a18404449f719749c6
akinyiliz <lizzieke84@gmail.com>
2023-01-04 14:21:44 +0300

    use shopcontext & display number of times a product is in cart

--- src/pages/shop/Product.jsx
+++ src/pages/shop/Product.jsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+import { ShopContext } from '../../context/ShopContext';
 
 function Product(props) {
 	const { id, productName, price, productImage } = props.data;
+	const { addToCart, cartItems } = useContext(ShopContext);
+
+	const cartItemAmount = cartItems[id];
+
 	return (
 		<div className="product">
 			<img
@@ -12,6 +18,12 @@ function Product(props) {
 				<p>{productName}</p>
 				<p>${price}</p>
 			</div>
+			<button
+				className="addToCartBttn"
+				onClick={() => addToCart(id)}
+			>
+				Add to cart {cartItemAmount > 0 && <>({cartItemAmount})</>}
+			</button>
 		</div>
 	);
 }
